be6a68f74f077bae6b25357dcc1f5d311e43bf55
Jaydenl6767 <Jaydenl321@nycstudents.net>
2025-12-08 09:26:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import requests
+"""import requests
 
 def getPoke(poke):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
@@ -14,5 +14,24 @@ def getPoke(poke):
         "types": [t["type"]["name"] for t in data["types"]]
     }
 
-pokemon = getPoke("Mew")
-print(pokemon)
+pokemon = getPoke("graveler")
+print(pokemon)"""
+
+import requests
+
+def getPotter(potter):
+    response = requests.get(f"https://vlaurencena.github.io/harry-potter-openapi-swagger-ui/#/")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return {
+        "characters": data["characters"],
+        "house": data["house"],
+        "spells": data["spells"],
+        "types": [t["type"]["characters"] for t in data["types"]]
+    }
+
+potter = getPotter("Harry Potter")
+print(potter)
